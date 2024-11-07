258281d361f26887ecf7be3011aedaeed30ca130
MurtozayevB <murtozayevbekzod7@gmail.com>
2024-11-07 09:49:43 +0500

    Murtazeyev Bekzod

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from heapq import heappush, heappop
 
+
 class Solution:
     def mergeKLists(self, lists: list[ListNode]) -> ListNode:
-        yordamchi = ListNode(0)  
-        joriy = yordamchi  
+        yordamchi = ListNode(0)
+        joriy = yordamchi
 
         ustuvorlik_navbati = []
-        
+
         for i, tugun in enumerate(lists):
-            if tugun:  
-                heappush(ustuvorlik_navbati, (tugun.val, i, tugun)) 
+            if tugun:
+                heappush(ustuvorlik_navbati, (tugun.val, i, tugun))
 
         while ustuvorlik_navbati:
             qiymat, i, tugun = heappop(ustuvorlik_navbati)
 
             joriy.next = tugun
-            joriy = joriy.next  
-            
+            joriy = joriy.next
+
             if tugun.next:
                 heappush(ustuvorlik_navbati, (tugun.next.val, i, tugun.next))
-        
+
         return yordamchi.next
